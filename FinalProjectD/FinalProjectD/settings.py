ba2c42bb19581c4c071230f8ed6ccf38bde07848
prossin18 <sangaleatul143@gmail.com>
2026-04-09 23:20:11 +0530

    create_custom app

--- FinalProjectD/FinalProjectD/settings.py
+++ FinalProjectD/FinalProjectD/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'harit_sena.apps.HaritSenaConfig',
 ]
 
 MIDDLEWARE = [
---
+++ FinalProjectD/harit_sena/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HaritSenaConfig(AppConfig):
+    name = 'harit_sena'
