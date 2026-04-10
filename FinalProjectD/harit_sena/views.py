d87cf842b586788e37def6217de4875084d4b414
Chinmayee259 <chinmayeeburte25@gmail.com>
2026-04-10 15:26:14 +0530

    register-rewards

--- FinalProjectD/harit_sena/views.py
+++ FinalProjectD/harit_sena/views.py
@@ -3,26 +3,26 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,index.html)
+    return render(request,'index.html')
 
 def contact(request):
-    return render(request,contact.html)
+    return render(request,'contact.html')
 
 def about(request):
-    return render(request,about.html)
+    return render(request,'about.html')
 
 def login(request):
-    return render(request,login.html)
+    return render(request,'login.html')
 
 def pickreq(request):
-    return render(request,pickreq.html)
+    return render(request,'pickreq.html')
 
 def register(request):
-    return render(request,register.html)
+    return render(request,'register.html')
 
 def request(request):
-    return render(request,request.html)
+    return render(request,'request.html')
 
 def rewards(request):
-    return render(request,rewards.html)
+    return render(request,'rewards.html')
 
